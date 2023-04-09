b418cd30fe388c34236131862a4c7af42edd1472
Eugen-str <eugen.01654@gmail.com>
2023-04-09 18:00:18 +0200

    Added Exercise4

---
+++ Chapter1/Exercise4.hs
@@ -0,0 +1,6 @@
+revqsort::Ord a=> [a] -> [a]
+revqsort [] = []
+revqsort (x:xs) = revqsort smaller ++ [x] ++ revqsort larger
+            where
+            smaller = [a | a <- xs, a > x]
+            larger = [b | b <- xs, b <= x]
